44a21aaa4a5d52aa469b853a705d6de3818d0c0a
reverendus <reverendus@invalid>
2017-06-09 11:21:04 +0200

    Missing empty line added

--- contracts/otc/SimpleMarket.py
+++ contracts/otc/SimpleMarket.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 class SimpleMarket:
     def __init__(self, web3, address):
         Market = web3.eth.contract(abi=abi, bytecode=bytecode)
