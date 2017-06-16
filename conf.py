f184c561824446a8cf2cd8f2fc54ae8e144b53c5
reverendus <reverendus@invalid>
2017-06-16 10:37:18 +0200

    Updated the config file for Sphinx documentation generation

--- conf.py
+++ conf.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# test1 documentation build configuration file, created by
-# sphinx-quickstart on Fri Jun 16 09:39:56 2017.
-#
 # This file is execfile()d with the current directory set to its
 # containing dir.
 #
@@ -16,7 +13,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
@@ -46,7 +43,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'test1'
+project = 'maker.py'
 copyright = '2017, MakerDAO'
 author = 'MakerDAO'
 
@@ -100,7 +97,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'test1doc'
+htmlhelp_basename = 'maker.py'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -127,7 +124,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'test1.tex', 'test1 Documentation',
+    (master_doc, 'maker.py.tex', 'maker.py Documentation',
      'MakerDAO', 'manual'),
 ]
 
@@ -137,7 +134,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'test1', 'test1 Documentation',
+    (master_doc, 'maker.py', 'maker.py Documentation',
      [author], 1)
 ]
 
@@ -148,8 +145,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'test1', 'test1 Documentation',
-     author, 'test1', 'One line description of project.',
+    (master_doc, 'maker.py', 'maker.py Documentation',
+     author, 'maker.py', 'One line description of project.',
      'Miscellaneous'),
 ]
 
