ad6f68e8ae9cc458f9b21356dea07c741124b142
reverendus <reverendus@invalid>
2017-06-29 15:15:01 +0200

    Network name detection introduced to keepers

--- keepers/Keeper.py
+++ keepers/Keeper.py
@@ -31,10 +31,10 @@ class Keeper:
         parser.add_argument("--eth-from", help="Ethereum account from which to send transactions", required=True, type=str)
         self.args(parser)
         self.arguments = parser.parse_args()
-        self.config = Config()
         self.web3 = Web3(HTTPProvider(endpoint_uri=f"http://{self.arguments.rpc_host}:{self.arguments.rpc_port}"))
         self.web3.eth.defaultAccount = self.arguments.eth_from #TODO allow to use ETH_FROM env variable
         self.our_address = Address(self.arguments.eth_from)
+        self.config = Config(self.web3)
 
         self.init()
 
