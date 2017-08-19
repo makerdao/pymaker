a570cbe8cf4b1cb1814e6cdc1f0bac058d8b1cd5
reverendus <reverendus@invalid>
2017-08-19 10:16:02 +0200

    Unused imports removed

--- api/test_sai.py
+++ api/test_sai.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import pytest
-
 from api import Address
 from api.conftest import SaiDeployment
 from api.feed import DSValue
