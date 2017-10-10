1b0cb1b42f639697d4d3b45069d126262b2736e1
reverendus <reverendus@invalid>
2017-10-10 20:11:02 +0200

    Unused import removed

--- keeper/sai.py
+++ keeper/sai.py
@@ -14,7 +14,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from web3 import Web3
 
 from keeper import Keeper
 from keeper.api import Address
