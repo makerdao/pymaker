dfe61d9f0e77e28e854dc43bd41f602c91929b56
reverendus <reverendus@invalid>
2017-11-30 19:56:19 +0100

    Unused imports removed

--- keeper/__init__.py
+++ keeper/__init__.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import _jsonnet
 import argparse
 import datetime
 import json
@@ -24,8 +23,6 @@ import signal
 import sys
 import threading
 import time
-
-import zlib
 
 import pkg_resources
 
