07331a9ce29e2f951edeecd0567f861426b77eb9
reverendus <reverendus@invalid>
2017-12-01 10:51:41 +0100

    Unit-tested specifying custom `gas` and `gas_buffer` in the `transact()` call

---
+++ tests/pymaker/test.py
@@ -0,0 +1,54 @@
+# This file is part of Maker Keeper Framework.
+#
+# Copyright (C) 2017 reverendus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from web3 import Web3, EthereumTesterProvider
+
+from pymaker import Address
+from pymaker.numeric import Wad
+from pymaker.token import DSToken
+
+
+class TestTransact:
+    def setup_method(self):
+        self.web3 = Web3(EthereumTesterProvider())
+        self.web3.eth.defaultAccount = self.web3.eth.accounts[0]
+        self.our_address = Address(self.web3.eth.defaultAccount)
+        self.second_address = Address(self.web3.eth.accounts[1])
+        self.token = DSToken.deploy(self.web3, 'ABC')
+        self.token.mint(Wad(1000000)).transact()
+
+    def test_default_gas(self):
+        # when
+        receipt = self.token.transfer(self.second_address, Wad(500)).transact()
+
+        # then
+        # [token transfer costs ~50k gas, we should add a 100k buffer by default which puts in the (100k, 200k) range]
+        assert 100000 <= self.web3.eth.getTransaction(receipt.transaction_hash)['gas'] <= 200000
+
+    def test_custom_gas(self):
+        # when
+        receipt = self.token.transfer(self.second_address, Wad(500)).transact(gas=129995)
+
+        # then
+        assert self.web3.eth.getTransaction(receipt.transaction_hash)['gas'] == 129995
+
+    def test_custom_gas_buffer(self):
+        # when
+        receipt = self.token.transfer(self.second_address, Wad(500)).transact(gas_buffer=3000000)
+
+        # then
+        assert self.web3.eth.getTransaction(receipt.transaction_hash)['gas'] > 3000000
