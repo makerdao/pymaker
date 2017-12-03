47e32041f3ec32b7b1a408b9bf6e4c4f28e6d671
reverendus <reverendus@invalid>
2017-12-03 09:58:26 +0100

    `account_levels_addr` changed to zero as otherwise tests will fail
    they would fail on any attempt to trade on EtherDelta

--- pymaker/deployment.py
+++ pymaker/deployment.py
@@ -87,7 +87,7 @@ class Deployment:
         etherdelta = EtherDelta.deploy(web3,
                                        admin=Address('0x1111100000999998888877777666665555544444'),
                                        fee_account=Address('0x8888877777666665555544444111110000099999'),
-                                       account_levels_addr=Address('0x6666655555444441111188888777770000099999'),
+                                       account_levels_addr=Address('0x0000000000000000000000000000000000000000'),
                                        fee_make=Wad.from_number(0.01),
                                        fee_take=Wad.from_number(0.02),
                                        fee_rebate=Wad.from_number(0.03))
