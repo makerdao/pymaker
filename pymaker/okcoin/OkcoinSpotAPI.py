85f26f98fc6092b6537a2ecd13963cc6899790bb
reverendus <reverendus@invalid>
2018-01-10 14:33:51 +0100

    Reorganizing the OKCoin example

--- pymaker/okcoin/OkcoinSpotAPI.py
+++ pymaker/okcoin/OkcoinSpotAPI.py
@@ -1,7 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #用于访问OKCOIN 现货REST API
-from .HttpMD5Util import buildMySign,httpGet,httpPost
+
+import http.client
+import urllib
+import json
+import hashlib
+
+def buildMySign(params,secretKey):
+    sign = ''
+    for key in sorted(params.keys()):
+        sign += key + '=' + str(params[key]) +'&'
+    data = sign+'secret_key='+secretKey
+    return  hashlib.md5(data.encode("utf8")).hexdigest().upper()
+
+def httpGet(url,resource,params=''):
+    conn = http.client.HTTPSConnection(url, timeout=10)
+    conn.request("GET",resource + '?' + params)
+    response = conn.getresponse()
+    data = response.read().decode('utf-8')
+    return json.loads(data)
+
+def httpPost(url,resource,params):
+    headers = {
+        "Content-type" : "application/x-www-form-urlencoded",
+    }
+    conn = http.client.HTTPSConnection(url, timeout=10)
+    temp_params = urllib.parse.urlencode(params)
+    conn.request("POST", resource, temp_params, headers)
+    response = conn.getresponse()
+    data = response.read().decode('utf-8')
+    params.clear()
+    conn.close()
+    return data
+
 
 class OKCoinSpot:
 
