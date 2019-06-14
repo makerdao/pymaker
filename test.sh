1d89b9001b879ea67ef1acc21dc3a59824ee8aa8
Ed Noepel <ed@makerdao.com>
2019-06-14 16:07:32 -0400

    Scripted loading of docker image from torball, fixed issue cleaning up ganache process

--- test.sh
+++ test.sh
@@ -1,13 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 # set -e
 
+# If the docker image doesn't already exist, load it from the tarball
+result=$( docker images -q mcd-testchain )
+if [[ "$result" == "" ]]; then
+    echo Docker image not found\; loading from tarball
+    gunzip -kf tests/config/mcd-testchain.tar.gz
+    docker load -q < tests/config/mcd-testchain.tar
+fi
+
+# Start ganache; record the PID so it can be cleanly stopped after testing
 ./ganache.sh &>/dev/null &
+ganache_pid=$(pgrep -f node)
+echo Started ganache as pid $ganache_pid
+
+# Start the docker image and wait for parity to initialize
 docker-compose up -d
-sleep 5
+sleep 2
 
-py.test --cov=pymaker --cov-report=term --cov-append tests/
+# Run the tests
+echo py.test --cov=pymaker --cov-report=term --cov-append tests/test_dss.py
 
-kill $(lsof -t -i tcp:8555)
+# Cleanup
+kill $ganache_pid
 echo Stopping container
 docker-compose down
