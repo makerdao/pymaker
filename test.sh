dbeff75085ba9890af43b7993c4fee3656f82e90
grandizzy <38490174+grandizzy@users.noreply.github.com>
2019-12-09 15:23:05 +0200

    Remove ganache tests dependency, move it in docker (#131)

--- test.sh
+++ test.sh
@@ -3,14 +3,14 @@
 # Pull the docker image
 docker pull makerdao/testchain-pymaker:unit-testing
 
-# Start ganache; record the PID so it can be cleanly stopped after testing
-command -v ganache-cli >/dev/null 2>&1 || { echo >&2 "Please install ganache before running tests"; exit 1; }
-./ganache.sh &>/dev/null &
-GANACHE_PID=$!
-echo Started ganache as pid $ganache_pid
+# Remove existing container if tests not gracefully stopped
+docker-compose down
 
-# Start the docker image and wait for parity to initialize
-docker-compose up -d
+# Start ganache
+docker-compose up -d ganache
+
+# Start parity and wait to initialize
+docker-compose up -d parity
 sleep 2
 
 # Run the tests
@@ -18,8 +18,6 @@ py.test --cov=pymaker --cov-report=term --cov-append tests/ $@
 TEST_RESULT=$?
 
 # Cleanup
-pkill -P $GANACHE_PID
-echo Stopping container
 docker-compose down
 
 exit $TEST_RESULT
