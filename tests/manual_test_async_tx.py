3fe65063cf8e1a713b10a55a2495ab1361af3dd5
Ed Noepel <ed@makerdao.com>
2020-03-31 14:39:17 -0400

    node-specific tx replacement gas thresholds

--- tests/manual_test_async_tx.py
+++ tests/manual_test_async_tx.py
@@ -56,10 +56,6 @@ class TestApp:
         self.wrap_amount = Wad(10)
 
     def main(self):
-        # tx_hash = '0x0e77dba683f10aee57114f6a753c33b7df5a2a99e63d7a1805e9e81f5694a917'
-        # encoded = Web3.toBytes(hexstr=tx_hash)
-        # assert len(encoded) == 32
-        # print(encoded)
         self.startup()
         self.test_replacement()
         self.shutdown()
@@ -77,7 +73,6 @@ class TestApp:
         second_tx = collateral.adapter.join(our_address, Wad(6))
         logging.info(f"Replacing first TX with legitimate gas price")
         second_tx.transact(replace=first_tx, gas_price=FixedGasPrice(2*GWEI))
-        # time.sleep(6)
 
     def shutdown(self):
         logging.info(f"Exiting {ilk.name} from our urn")
