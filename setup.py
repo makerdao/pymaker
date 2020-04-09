f71ccef0ebe56aa0c990fc516ece840e243bd22e
daimesava <daimesava@users.noreply.github.com>
2020-04-09 19:31:33 +0300

    skip unnecessary io.open import

--- setup.py
+++ setup.py
@@ -9,11 +9,6 @@ https://github.com/pypa/sampleproject/blob/master/setup.py
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from os import path
-# io.open is needed for projects that support Python 2.7
-# It ensures open() defaults to text mode with universal newlines,
-# and accepts an argument to specify the text encoding
-# Python 3 only projects can skip this import
-from io import open
 
 here = path.abspath(path.dirname(__file__))
 
