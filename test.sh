248ee183573535fe3c49d22455b2ad50bcfc1c3f
Ed Noepel <ed@makerdao.com>
2021-04-05 14:58:55 -0400

    fixed issue with flop test and implemented a hack to enable the faucet

--- test.sh
+++ test.sh
@@ -13,6 +13,12 @@ docker-compose up -d ganache
 docker-compose up -d parity
 sleep 2
 
+# HACK: Enable the token faucet
+export DEPLOYMENT_ADDRESS=0x00a329c0648769A73afAc7F9381E08FB43dBEA72
+export FAUCET=0x0A64DF94bc0E039474DB42bb52FEca0c1d540402
+seth send -F $DEPLOYMENT_ADDRESS -S /dev/null --keystore=tests/config/keys/UnlimitedChain \
+  $FAUCET 'hope(address)' 0x0000000000000000000000000000000000000000
+
 # Run the tests
 py.test --cov=pymaker --cov-report=term --cov-append tests/ $@
 TEST_RESULT=$?
